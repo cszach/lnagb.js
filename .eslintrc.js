79d76ceacb7659e646d61135f3814a7104a1890d
Nguyen Hoang Duong <you_create@protonmail.com>
2020-01-08 19:50:22 +0700

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es6": true,
+		"node": true
+	},
+	"extends": "mdcs",
+	"globals": {
+		"Atomics": "readonly",
+		"SharedArrayBuffer": "readonly"
+	},
+	"parserOptions": {
+		"ecmaVersion": 2018
+	},
+	"rules": {
+	}
+};
