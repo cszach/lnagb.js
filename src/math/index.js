4be7e1b151223444f2a62fa2f2fd77015a592d9b
Nguyen Hoang Duong <you_create@protonmail.com>
2020-04-29 17:19:31 +0700

    math: Create index.js file

---
+++ src/math/index.js
@@ -0,0 +1,4 @@
+export { MathUtils } from './MathUtils';
+export { LinearEquation } from './LinearEquation.js';
+export { LinearEquation1, LinearEquation2, LinearEquation3 } from './LinearEquations.js';
+export { SystemOfLinearEquations } from './SystemOfLinearEquations.js';
