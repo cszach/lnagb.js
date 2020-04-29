1e46223cc8d00300006e6eee9f1eb4b15b67cbd9
Nguyen Hoang Duong <you_create@protonmail.com>
2020-04-29 17:19:59 +0700

    Add script to build components of lnagb.js

---
+++ rollup-components.config.js
@@ -0,0 +1,26 @@
+let components = [ 'matrices', 'math' ];
+let exports = [];
+
+components.forEach( ( component ) => {
+
+	exports.push(
+		{
+			input: `src/${component}/index.js`,
+			output: {
+				name: "lnagbjs",
+				file: `build/components/${component}/lnagb.${component}.js`,
+				format: "umd"
+			}
+		},
+		{
+			input: `src/${component}/index.js`,
+			output: {
+				file: `build/components/${component}/lnagb.${component}.module.js`,
+				format: "esm"
+			}
+		}
+	);
+
+} );
+
+export default exports;
