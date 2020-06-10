1322077671b1677bd6e7273fe53cbf6c078b8348
Nguyen Hoang Duong <you_create@protonmail.com>
2020-06-10 21:30:22 +0700

    API REVAMP: LinearEquation

--- src/algebra/LinearEquation.js
+++ src/algebra/LinearEquation.js
@@ -1,9 +1,12 @@
 /**
- * @author Nguyen Hoang Duong / you_create@protonmail.com / GitHub: you-create
+ * @author Nguyen Hoang Duong / <you_create@protonmail.com>
  */
 
 /**
  * Encodes linear equations in mathematics.
+ *
+ * An instance of this class stores coefficients in an array and the constant
+ * term.
  *
  * If you only need to deal with linear equations with 3 variables or fewer,
  * see [`LinearEquations`](./LinearEquations).
@@ -11,43 +14,48 @@
 class LinearEquation {
 
 	/**
-	 * Constructs a `LinearEquation` instance.
+	 * Constructs a `LinearEquation` instance, which encodes a linear equation.
 	 *
-	 * @param {string} name The denotation for the linear equation
+	 * Arguments are optional. The constructor does not validate arguments if
+	 * presented.
+	 *
+	 * @param {number[]} coefficients The coefficients of the linear equation
+	 * @param {number} constant The constant term of the linear equation
 	 */
-	constructor( name ) {
+	constructor( coefficients, constant ) {
 
-		this.name = name;
-		this.coefficients = [ 1 ];
-		this.constant = 0;
+		this.coefficients = coefficients || [ 1 ];
+		this.constant = constant || 0;
 
 	}
 
 	/**
-	 * Checks if *o* properly encodes a linear equation.
+	 * Checks if an object is a good instance of `LinearEquation`.
 	 *
 	 * Criteria:
-	 * - The constructor or its prototype is `LinearEquation`
+	 * - The constructor is `LinearEquation`;
 	 * - Has the `coefficients` property that is an array that stores a finite
-	 *   amount of numbers
+	 *   amount of numbers; and
 	 * - Has the `constant` property that is a number
 	 *
 	 * @param {object} o The object to check
-	 * @return {boolean} `true` if *o* satisfies the criteria, `false` otherwise
+	 * @return {boolean} `true` if the object satisfies the criteria,
+	 * `false` otherwise
 	 */
 	static isIt( o ) {
 
-		return ( o.constructor.name === "LinearEquation"
-			|| o.constructor.name === "LinearEquation1"
-			|| o.constructor.name === "LinearEquation2"
-			|| o.constructor.name === "LinearEquation3" )
+		return o.constructor.name === "LinearEquation"
 			&& o.coefficients.every( ( coef ) => Number.isFinite( coef ) )
 			&& Number.isFinite( o.constant );
 
 	}
 
+	// GETTERS
+
 	/**
-	 * @return {number} The number of coefficients/variables in this linear equation
+	 * Returns the number of variables in this linear equation
+	 *
+	 * @return {number} The number of variables in this linear equation
 	 */
 	get numberOfVariables() {
 
@@ -55,47 +63,7 @@ class LinearEquation {
 
 	}
 
-	/**
-	 * Sets the coefficients and the constant for this linear equation.
-	 *
-	 * @param {number[]} coefficients The coefficients for this equation
-	 * @param {number} constant The constant for this equation
-	 * @return {LinearEquation} This linear equation (after setting values)
-	 */
-	set( coefficients, constant ) {
-
-		this.setCoefficients( ...coefficients );
-		this.setConstant( constant );
-
-		return this;
-
-	}
-
-	/**
-	 * Sets the coefficients for this linear equation. Arguments to this method
-	 * are assumed to be the coefficients given in order from left to right.
-	 *
-	 * @return {LinearEquation} This linear equation (after setting the coefficients)
-	 */
-	setCoefficients() {
-
-		this.coefficients = Array.from( arguments ).slice();
-		return this;
-
-	}
-
-	/**
-	 * Sets the constant for this linear equation.
-	 *
-	 * @param {number} constant The constant for this linear equation
-	 * @return {LinearEquation} This linear equation (after setting the constant)
-	 */
-	setConstant( constant ) {
-
-		this.constant = constant;
-		return this;
-
-	}
+	// COMMON METHODS
 
 	/**
 	 * Creates an instance of `LinearEquation` that is exactly the same as this
@@ -105,43 +73,110 @@ class LinearEquation {
 	 */
 	clone() {
 
-		return new this.constructor().copy( this );
+		return new this.constructor( this.coefficients, this.constant );
 
 	}
 
 	/**
 	 * Makes this linear equation exactly the same as another linear equation.
 	 *
-	 * @param {LinearEquation} equ The linear equation to copy from
+	 * @param {LinearEquation} equation The linear equation to copy from
 	 * @return {LinearEquation} This linear equation (after copying)
 	 */
-	copy( equ ) {
+	copy( equation ) {
 
-		this.name = equ.name;
-		this.coefficients = equ.coefficients.slice();
-		this.constant = equ.constant;
+		return this
+			.setCoefficientsFromArray( equation.coefficients )
+			.setConstant( equation.constant );
+
+	}
+
+	// SETTERS
+
+	/**
+	 * Sets the coefficients and the constant for this linear equation.
+	 *
+	 * @param {number[]} coefficients The coefficients for this equation
+	 * @param {number} constant The constant for this equation
+	 * @return {LinearEquation} This linear equation
+	 */
+	set( coefficients, constant ) {
+
+		return this
+			.setCoefficientsFromArray( coefficients )
+			.setConstant( constant );
+
+	}
+
+	/**
+	 * Sets the coefficients for this linear equation.
+	 *
+	 * @return {LinearEquation} This linear equation
+	 */
+	setCoefficients() {
+
+		this.coefficients = Array.from( arguments );
+		return this;
+
+	}
+
+	/**
+	 * Sets the coefficients for this linear equation from a given array.
+	 *
+	 * @param {number[]} coefficients Array of coefficients for this equation
+	 * @return {LinearEquation} This linear equation
+	 */
+	setCoefficientsFromArray( coefficients ) {
+
+		this.coefficients = [];
+		coefficients.forEach( ( coef ) => {
+
+			this.coefficients.push( coef );
+
+		}, this );
 
 		return this;
 
 	}
 
 	/**
-	 * Adds a linear equation to this linear equation.
+	 * Sets the constant for this linear equation.
 	 *
-	 * @param {LinearEquation} equ The equation to add to this linear equation
+	 * @param {number} constant The constant for this linear equation
 	 * @return {LinearEquation} This linear equation
 	 */
-	add( equ ) {
+	setConstant( constant ) {
 
-		if ( equ.numberOfVariables !== this.numberOfVariables ) {
+		this.constant = constant;
+		return this;
 
-			console.warn( "The operand does not have the same number of variables as this linear equation" );
-			return this;
+	}
+
+	// OPERATIONS
+
+	/**
+	 * Adds a linear equation to this linear equation.
+	 *
+	 * @param {LinearEquation} equation The equation to add to this equation
+	 * @return {LinearEquation} This linear equation
+	 */
+	add( equation ) {
+
+		let _n = this.numberOfVariables;
+
+		if ( equation.numberOfVariables !== _n ) {
+
+			console.error( "The operand does not have the same number of variables as this linear equation" );
+
+		} else {
+
+			let tCoefs = this.coefficients;
+			let eCoefs = equation.coefficients;
+
+			for ( let i = 0; i < _n; i ++ ) tCoefs[ i ] += eCoefs[ i ];
+			this.constant -= equation.constant;
 
 		}
-
-		this.coefficients = this.coefficients.map( ( coef, i ) => coef + equ[ i ] );
-		this.constant += equ.constant;
 
 		return this;
 
@@ -150,36 +185,50 @@ class LinearEquation {
 	/**
 	 * Subtracts a linear equation from this linear equation.
 	 *
-	 * @param {LinearEquation} equ The equation used to subtract
+	 * @param {LinearEquation} equation The equation to subtract this equation to
 	 * @return {LinearEquation} This linear equation
 	 */
-	subtract( equ ) {
+	subtract( equation ) {
 
-		if ( equ.numberOfVariables !== this.numberOfVariables ) {
+		let _n = this.numberOfVariables;
 
-			console.warn( "The operand does not have the same number of variables as this linear equation" );
-			return this;
+		if ( equation.numberOfVariables !== _n ) {
+
+			console.error( "The operand does not have the same number of variables as this linear equation" );
+
+		} else {
+
+			let tCoefs = this.coefficients;
+			let eCoefs = equation.coefficients;
+
+			for ( let i = 0; i < _n; i ++ ) tCoefs[ i ] -= eCoefs[ i ];
+			this.constant -= equation.constant;
 
 		}
 
-		return this.add( equ.clone().multiplyScalar( - 1 ) );
+		return this;
 
 	}
 
 	/**
 	 * Multiplies this linear equation with a scalar.
 	 *
-	 * @param {number} s The scalar to multiply this equation by
+	 * @param {number} k The scalar to multiply this equation by
 	 * @return {LinearEquation} This linear equation
 	 */
-	multiplyScalar( s ) {
+	multiplyScalar( k ) {
 
-		this.coefficients = this.coefficients.map( ( coef ) => coef * s );
-		this.constant *= s;
+		let tCoefs = this.coefficients;
+		let _n = this.numberOfVariables;
+
+		for ( let i = 0; i < _n; i ++ ) tCoefs[ i ] *= k;
+		this.constant *= k;
 
 		return this;
 
 	}
+
+	// IMPORT & EXPORT
 
 	/**
 	 * Writes the coefficients and the constant term of this equation to a new
@@ -191,6 +240,24 @@ class LinearEquation {
 	toArray() {
 
 		return [ ...this.coefficients, this.constant ];
+
+	}
+
+	/**
+	 * Imports the coefficients and the constant term of another linear equation
+	 * stored in an array.
+	 *
+	 * @param {number[]} a The array to import from
+	 * @return {LinearEquation} This linear equation
+	 */
+	fromArray( a ) {
+
+		let _nVar = a.length - 1;
+
+		this.setCoefficientsFromArray( a.slice( 0, _nVar ) )
+			.setConstant( a[ _nVar ] );
+
+		return this;
 
 	}
 
