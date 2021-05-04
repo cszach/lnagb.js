90475ea500235eff553c2fa29f2f2bb501cea018
Nguyen Hoang Duong <you_create@protonmail.com>
2021-05-04 23:20:56 +0700

    matrices: Rename BaseMatrix to CommonMatrix

--- src/matrices/CommonMatrix.js
+++ src/matrices/CommonMatrix.js
@@ -1,10 +1,32 @@
 import { sameSize, equal } from './MatrixUtils.js';
 
 /**
- * Stores implementations of operations that are shared across different matrix
- * classes.
+ * @module CommonMatrix
+ * @author Nguyen Hoang Duong / <you_create@protonmail.com>
+ * @description
+ *
+ * Contains the {@link module:CommonMatrix~CommonMatrix} class.
+ *
  */
-class BaseMatrix {
+
+/**
+ * Stores implementations of common matrix operations for reference in any
+ * matrix class development.
+ */
+class CommonMatrix {
+
+	/**
+	 * Returns the number of entries in this matrix.
+	 *
+	 * @return {number} Number of entries in this matrix
+	 */
+	get numberOfEntries() {
+
+		let _size = this.size;
+
+		return _size.rows * _size.columns;
+
+	}
 
 	get rank() {
 
@@ -42,12 +64,25 @@ class BaseMatrix {
 
 	}
 
+	/**
+	 * Checks if this matrix has the same size as another matrix.
+	 *
+	 * @param {Matrix} matrix The matrix to check the size of this matrix against.
+	 * @return {boolean} `true` if the two matrices have the same size, `false`
+	 * otherwise
+	 */
 	sameSize( matrix ) {
 
 		return sameSize( this, matrix );
 
 	}
 
+	/**
+	 * Checks if this matrix and another matrix are equal.
+	 *
+	 * @param {Matrix} matrix The matrix to compare this matrix to.
+	 * @return {boolean} `true` if the two matrices are the same, `false` otherwise
+	 */
 	equals( matrix ) {
 
 		return equal( this, matrix );
@@ -72,7 +107,7 @@ class BaseMatrix {
 	 * Converts a 0-based index of an element in this matrix to its 1-indexed
 	 * row number.
 	 *
-	 * @param {number} index The index to convert (0-based)
+	 * @param {number} index The index to convert (0-based).
 	 * @return {number} The index's row number (1-indexed)
 	 */
 	indexToRow( index ) {
@@ -85,7 +120,7 @@ class BaseMatrix {
 	 * Converts a 0-based index of an element in this matrix to its 1-indexed
 	 * column number.
 	 *
-	 * @param {number} index The index to convert (0-based)
+	 * @param {number} index The index to convert (0-based).
 	 * @return {number} The index's column number (1-indexed)
 	 */
 	indexToColumn( index ) {
@@ -96,4 +131,4 @@ class BaseMatrix {
 
 }
 
-export { BaseMatrix };
+export { CommonMatrix };
