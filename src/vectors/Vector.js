cc8b24782425d7e3fba4b26be4a37b4e6a441395
Nguyen Hoang Duong <you_create@protonmail.com>
2021-05-11 09:27:10 +0700

    vectors/Vector.js: Add class methods 'multiplyScalar' and 'negate'

--- src/vectors/Vector.js
+++ src/vectors/Vector.js
@@ -1,9 +1,18 @@
 /**
+ * @module Vector
  * @author Nguyen Hoang Duong / <you_create@protonmail.com>
+ * @description
+ *
+ * Contains the {@link module:Vector~Vector} class, which encodes vector
+ * quantities in linear algebra.
  */
 
 /**
- * Encodes vectors and vector operations.
+ * Encodes vectors and vector operations. Compatible for use with matrices (e.g.
+ * for matrix-vector multiplications).
+ *
+ * For optimal performance, use {@link module:Vector2~Vector2},
+ * {@link module:Vector3~Vector3}, {@link module:Vector4~Vector4} if possible.
  */
 class Vector {
 
@@ -18,6 +27,22 @@ class Vector {
 	clone() {
 
 		return new this.constructor( this.elements.slice() );
+
+	}
+
+	multiplyScalar( k ) {
+
+		let _ = this.elements;
+
+		for ( let i = 0, _n = this.numberOfEntries; i < _n; i ++ ) _[ i ] *= k;
+
+		return this;
+
+	}
+
+	negate() {
+
+		return this.multiplyScalar( - 1 );
 
 	}
 
