c5c7f6a4dd4789582769991e091e7907ae9c96e5
Nguyen Hoang Duong <you_create@protonmail.com>
2021-05-14 22:43:08 +0700

    algebra/Algebra.js: Update file structure

--- src/algebra/Algebra.js
+++ src/algebra/Algebra.js
@@ -4,21 +4,16 @@
  * @description
  *
  * Contains algebraic operations and several functions for dealing with algebra.
- * Helpful for the lnagb.js library itself as well as potential users.
  *
  */
-
-function Algebra() {}
 
 /**
  * Computes and returns the result of the linear combination of 2 sequences
  * of numbers. The sequences must have the same number of terms.
  *
- * This is useful in multiplying matrices.
- *
  * @param {number[]} p First sequence of numbers.
  * @param {number[]} q Second sequence of numbers.
- * @return {number} The result of the linear combination of the two sequences
+ * @returns {number} The result of the linear combination of the two sequences
  *
  * @example
  * ```javascript
@@ -30,10 +25,10 @@ function Algebra() {}
  * // ( 1 * - 9 ) + ( 2 * - 8 ) + ( 3 * - 7 ) = - 46
  * ```
  */
-Algebra.linearCombination = function ( p, q ) {
+function linearCombination( p, q ) {
 
 	return p.reduce( ( result, v, i ) => result + p[ i ] * q[ i ], 0 );
 
-};
+}
 
-export { Algebra };
+export { linearCombination };
