26c5fe47f756a9d7024f0fbf753d8fa9f354d578
Nguyen Hoang Duong <you_create@protonmail.com>
2021-06-03 09:46:27 +0700

    matrices/AugmentedMatrix.js: Add docs and getters

--- src/matrices/AugmentedMatrix.js
+++ src/matrices/AugmentedMatrix.js
@@ -1,3 +1,5 @@
+import { Matrix } from './Matrix.js';
+
 /**
  * @module AugmentedMatrix
  * @author Nguyen Hoang Duong / <you_create@protonmail.com>
@@ -11,8 +13,16 @@
 /**
  * Encodes augmented matrices and their operations in Linear Algebra.
  *
+ * This class inherits class methods from {@link module:Matrix~Matrix} with the
+ * exception that `transpose`, `add`, `sub`, and `multiply` cannot be used.
+ *
+ * The `size` property has two more elements: `left`—which is the number of
+ * columns of the left matrix—and `right`—that of the right matrix.
+ *
+ * @augments module:Matrix~Matrix
+ * @see {@link module:Matrix~Matrix} for common properties
  */
-class AugmentedMatrix {
+class AugmentedMatrix extends Matrix {
 
 	/**
 	 * Constructs an `AugmentedMatrix` instance, which encodes an augmented matrix.
@@ -23,41 +33,75 @@ class AugmentedMatrix {
 	 */
 	constructor( left, right ) {
 
-		/**
-		 * @member {object}
-		 * @description The left part of this augmented matrix.
-		 */
-		this.left = left.clone();
-
-		/**
-		 * @member {object}
-		 * @description The right part of this augmented matrix.
-		 */
-		this.right = right.clone();
-
 		let leftSize = left.size;
 		let rightSize = right.size;
 
-		this.size = {
-			rows: leftSize.rows,
-			columns: leftSize.columns + rightSize.columns
-		};
+		let _nRows = leftSize.rows;
+		let leftSizeCols = leftSize.columns;
+		let rightSizeCols = rightSize.columns;
 
-		this.numberOfEntries = left.numberOfEntries + right.numberOfEntries;
+		let elements = [];
+
+		for ( let i = 1, __nRows = _nRows + 1; i < __nRows; i ++ )
+			elements.push( ...left.row( i ), ...right.row( i ) );
+
+		super( _nRows, leftSizeCols + rightSizeCols, elements );
+
+		this.size.left = leftSizeCols;
+		this.size.right = rightSizeCols;
 
 	}
 
 	/**
-	 * Creates and returns a clone of this augmented matrix instance.
+	 * Returns the left matrix of this augmented matrix.
 	 *
-	 * @returns {AugmentedMatrix} A clone of this instance
+	 * @returns {module:Matrix~Matrix} The left matrix
 	 */
-	clone() {
+	get leftMatrix() {
 
-		return new this.constructor( this.left, this.right );
+		let _size = this.size;
+		let _nRows = _size.rows;
+		let leftSizeCols = _size.left;
+
+		let elements = [];
+
+		for ( let i = 1; i <= _nRows; i ++ )
+			for ( let j = 1; j <= leftSizeCols; j ++ )
+				elements.push( this.entry( i, j ) );
+
+		return new Matrix( _nRows, leftSizeCols, elements );
+
+	}
+
+	/**
+	 * Returns the right matrix of this augmented matrix.
+	 *
+	 * @returns {module:Matrix~Matrix} The right matrix
+	 */
+	get rightMatrix() {
+
+		let _size = this.size;
+		let _nRows = _size.rows;
+		let leftSizeCols = _size.left;
+		let rightSizeCols = _size.right;
+
+		let elements = [];
+
+		for ( let i = 1; i <= _nRows; i ++ )
+			for ( let j = 1; j <= rightSizeCols; j ++ )
+				elements.push( this.entry( i, leftSizeCols + j ) );
+
+		return new Matrix( _nRows, rightSizeCols, elements );
 
 	}
 
 }
+
+// Disable these class methods for augmented matrices
+
+AugmentedMatrix.prototype.transpose = undefined;
+AugmentedMatrix.prototype.add = undefined;
+AugmentedMatrix.prototype.sub = undefined;
+AugmentedMatrix.prototype.multiply = undefined;
 
 export { AugmentedMatrix };
